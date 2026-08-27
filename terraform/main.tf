f7a09d175bb7666d7939274d7ce30c2e90aa4799
Michael Thomas <25michaelthomas@gmail.com>
2026-08-27 19:24:02 -0400

    fmt test

---
+++ terraform/main.tf
@@ -0,0 +1,24 @@
+terraform {
+    required_version = "~>1.11"
+    required_providers {
+      aws = {
+        source = "hashicorp/aws"
+        version = "~>5.0"
+      }
+      random = {
+        source = "hashicorp/random"
+        version = "~>3.5"
+      }
+    }
+    backend "s3" {
+      bucket = "8696-tfstate-portfolio"
+      key = "app4/terraform.tfstate"
+      region = "us-east-1"
+      use_lockfile = true
+      encrypt = true
+    }
+}
+
+provider "aws" {
+    region = "us-east-1"
+}
