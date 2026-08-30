daffc3d497775cb782717b5451c5bade3791e38a
Michael Thomas <25michaelthomas@gmail.com>
2026-08-30 13:16:04 -0400

    testing apply again

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,7 +24,7 @@ provider "aws" {
 }
 
 resource "aws_vpc" "github-actions" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
-  enable_dns_support = true
+  enable_dns_support   = true
 }
